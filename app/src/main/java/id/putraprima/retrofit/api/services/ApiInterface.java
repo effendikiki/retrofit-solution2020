d497ded2c4671b548c25e0488a3b2e6dac20b1fc
effendikiki <raito12cs@gmail.com>
2020-04-04 20:11:22 +0700

    menyelesaikan error handling

--- app/src/main/java/id/putraprima/retrofit/api/services/ApiInterface.java
+++ app/src/main/java/id/putraprima/retrofit/api/services/ApiInterface.java
@@ -5,6 +5,8 @@ import id.putraprima.retrofit.api.models.AppVersion;
 import id.putraprima.retrofit.api.models.Envelope;
 import id.putraprima.retrofit.api.models.LoginRequest;
 import id.putraprima.retrofit.api.models.LoginResponse;
+import id.putraprima.retrofit.api.models.RegisterRequest;
+import id.putraprima.retrofit.api.models.RegisterResponse;
 import id.putraprima.retrofit.api.models.UpdatePasswordRequest;
 import id.putraprima.retrofit.api.models.UpdatePasswordResponse;
 import id.putraprima.retrofit.api.models.UpdateProfileRequest;
@@ -17,12 +19,17 @@ import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 
 public interface ApiInterface{
+
+//    endpoint : https://mobile.putraprima.id
+
     @GET("/")
     Call<AppVersion> getAppVersion();
 
     @POST("/api/auth/login")
     Call<LoginResponse> doLogin(@Body LoginRequest loginRequest);
 
+    @POST("/api/auth/register")
+    Call<Envelope<RegisterResponse>> doRegister(@Body RegisterRequest registerRequest);
 
     @GET("/api/auth/me")
     Call<Envelope<UserInfo>> me();
